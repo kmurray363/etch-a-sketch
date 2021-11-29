5ed2aad28f356eff935279d9ca921e64318faee8
Kyle <kmurray363@gmail.com>
2021-11-29 18:12:09 -0500

    Edit 2:
    - added ability to change grid size
    - clear button is still non-functional

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const body = document.querySelector('body');
-const gridContainer = document.createElement('div');
-gridContainer.classList.add("gridContainer");
-body.appendChild(gridContainer);
+const clearButton = document.createElement('button');
+let gridContainer = document.createElement('div');
 
+clearButton.classList.add('clearButton');
+body.appendChild(clearButton);
+
+let counter = 0;
 
 function createGrid(gridSize){
+    console.log(counter);
+    if(counter = 1){gridContainer.remove()};
+    counter = 1;
+    gridContainer = document.createElement('div');
+    gridContainer.setAttribute('id',"gridContainer");
+    body.appendChild(gridContainer);
+
     for(let y = 0; y < gridSize; y++){
         const row = document.createElement('div');
         row.classList.add(`row`, `y${y}`);
@@ -12,6 +22,7 @@ function createGrid(gridSize){
         for(let x = 0; x < gridSize; x++){
             const column = document.createElement('div');
             column.classList.add(`column`, `x${x}`);
+            column.style.width = `${960/gridSize}px`;
             column.onmouseover = () => {
                 column.style.backgroundColor = 'black';
             };
@@ -20,12 +31,5 @@ function createGrid(gridSize){
     };
 }
 
-function createClearButton(){
-    const clearButton = document.createElement('button');
-    clearButton.classList.add('clearButton');
-    gridContainer.appendChild('clearButton');
-}
 
-
-createGrid(160);
-createClearButton();
+clearButton.addEventListener('click',createGrid(prompt('Please enter a grid size.', 100)));
